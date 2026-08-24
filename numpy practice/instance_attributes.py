bfcbd8a3c9781865996776bd68f5abe62caca24c
minahilmasood1 <minahilmasood80@gmail.com>
2026-08-24 12:22:38 +0500

    Update instance_attributes.py

--- numpy practice/instance_attributes.py
+++ numpy practice/instance_attributes.py
@@ -1,1 +1,11 @@
+import numpy as np
+array = np.array([[1,2,3,4,5],
+                  [6,7,8,9,10],
+                  [11,12,13,14,15]])
+print(array.shape)
+print(array.dtype)
+print(array.ndim)
+print(array.itemsize)
+print(array.nbytes)
+print(array.T)
 
