d38413e384280243a37cbf3af0c55950ea717e6c
minahilmasood1 <minahilmasood80@gmail.com>
2026-08-24 12:45:21 +0500

    Create numpy_functions.py

---
+++ numpy practice/numpy_functions.py
@@ -0,0 +1,14 @@
+import numpy as np
+array = np.zeros((2,3))
+array_2 = np.ones((4,5))
+array_3 = np.full((2,3),5)
+array_4 = np.linspace(0,2,5)
+array_4 = np.arange(0,10,2)
+array_5 = np.eye(2) #identity matrix
+array_6 = np.diag([3,2,6])
+print(array)
+print(array_2)
+print(array_3)
+print(array_4)
+print(array_5)
+print(array_6)
